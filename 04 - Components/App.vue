75876cc29d8a2bcf292ffb44cdde35bc88420bc6
Ahmed Samy <accountsworlds@gmail.com>
2026-03-26 15:24:41 +0000

    Add App.vue with input and MyComponent usage

---
+++ 04 - Components/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+import MyComponent from './MyComponent.vue'
+
+const msg = ref('Text')
+</script>
+
+
+
+<template>
+
+<input placeholder="text please" v-model="msg" />
+
+  <MyComponent title="Welcome" sub="ahmed" />
+  <MyComponent title="Features" sub="toto"/>
+  <MyComponent :sub="msg" title="msg" />
+</template>
+
