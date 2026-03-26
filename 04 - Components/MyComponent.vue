aaeb4262838f444a17e773d080a89717a55631ea
Ahmed Samy <accountsworlds@gmail.com>
2026-03-26 15:25:16 +0000

    Add MyComponent.vue with props and styles

---
+++ 04 - Components/MyComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps(['title','sub'])
+</script>
+
+<template>
+  <div class="card">
+    <h3>{{ title }}</h3>
+    <p>{{sub}}</p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  border-top: 7px solid #AAAAFF;
+  padding: 10px;
+  margin: 5px 0;
+  
+}
+</style>
